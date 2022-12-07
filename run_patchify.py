ddd9250ba8529731faacf1b286a2ae6dbd0d18e0
IshmanM <ishman.mann7@gmail.com>
2022-12-06 19:08:46 -0700

    code cleanup

--- run_patchify.py
+++ run_patchify.py
@@ -19,25 +19,29 @@ from utils import patchify_images
 from dotenv import load_dotenv
 import os
 
+
+load_dotenv('.env')
+
+# Hyperpetameters and globals
+
+PATCH_WIDTH = int(os.environ["PATCH_WIDTH"])
+PATCH_HEIGHT = int(os.environ["PATCH_HEIGHT"])
+PATCH_SIZE = (PATCH_WIDTH, PATCH_HEIGHT)
+
+STEP = 1000
+
+IMAGE_SAVE_TYPE = os.environ["IMAGE_SAVE_TYPE"]
+MASK_SAVE_TYPE = os.environ["MASK_SAVE_TYPE"]
+
+PATCHIFY_QUEUE = [{"name":"TRAIN_IMAGES", "save_type":IMAGE_SAVE_TYPE}, 
+                  {"name":"TRAIN_MASKS", "save_type":MASK_SAVE_TYPE}, 
+                  {"name":"TEST_IMAGES", "save_type":IMAGE_SAVE_TYPE}, 
+                  {"name":"TEST_MASKS", "save_type":MASK_SAVE_TYPE}, 
+                  {"name":"VALIDATION_IMAGES", "save_type":IMAGE_SAVE_TYPE}, 
+                  {"name":"VALIDATION_MASKS", "save_type":MASK_SAVE_TYPE}]
+                  
+
 if __name__ == "__main__":
-
-    load_dotenv('.env')
-
-    PATCH_WIDTH = int(os.environ["PATCH_WIDTH"])
-    PATCH_HEIGHT = int(os.environ["PATCH_HEIGHT"])
-    PATCH_SIZE = (PATCH_WIDTH, PATCH_HEIGHT)
-    
-    STEP = 1000
-
-    IMAGE_SAVE_TYPE = os.environ["IMAGE_SAVE_TYPE"]
-    MASK_SAVE_TYPE = os.environ["MASK_SAVE_TYPE"]
-
-    PATCHIFY_QUEUE = [{"name":"TRAIN_IMAGES", "save_type":IMAGE_SAVE_TYPE}, 
-                      {"name":"TRAIN_MASKS", "save_type":MASK_SAVE_TYPE}, 
-                      {"name":"TEST_IMAGES", "save_type":IMAGE_SAVE_TYPE}, 
-                      {"name":"TEST_MASKS", "save_type":MASK_SAVE_TYPE}, 
-                      {"name":"VALIDATION_IMAGES", "save_type":IMAGE_SAVE_TYPE}, 
-                      {"name":"VALIDATION_MASKS", "save_type":MASK_SAVE_TYPE}]
                       
     for image_set in PATCHIFY_QUEUE:
         
